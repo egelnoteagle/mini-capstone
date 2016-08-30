8468712ca9f348575d2e9091929b2513633573c8
degel55 <chicubfan@gmail.com>
2016-08-29 22:50:10 -0500

    finished adding all products to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,25 +20,52 @@ Product.create(artist: "Radiohead",
                  album: "In Rainbows",
                  year: "2007",
                  weight: "120 grams",
-                 genre: "Rock",
+                 genre: "Alternative",
                  price: "$18.99",
                  image: "https://images-na.ssl-images-amazon.com/images/I/61gGZcwziIL.jpg"
                  )
 
-Product.create(artist: "Beastie Boys",
-                 album: "Paul's Boutique",
-                 year: "1989",
-                 weight: "180 grams",
-                 genre: "Rap",
-                 price: "$21.99",
-                 image: "https://upload.wikimedia.org/wikipedia/en/0/07/BeastieBoysPaul%27sBoutique.jpg"
+Product.create(artist: "The War on Drugs",
+                 album: "Lost In The Dream",
+                 year: "2014",
+                 weight: "120 grams",
+                 genre: "Alternative",
+                 price: "$19.78",
+                 image: "https://images-na.ssl-images-amazon.com/images/I/81haPiAN3JL._SL1425_.jpg"
                  )
 
-Product.create(artist: "Beastie Boys",
-                 album: "Paul's Boutique",
-                 year: "1989",
+Product.create(artist: "Miles Davis",
+                 album: "Kind of Blue",
+                 year: "1959",
                  weight: "180 grams",
+                 genre: "Jazz",
+                 price: "$11.44",
+                 image: "https://images-na.ssl-images-amazon.com/images/I/81mHq1buc5L._SL1500_.jpg"
+                 )
+
+Product.create(artist: "Disclosure",
+                 album: "Settle",
+                 year: "2013",
+                 weight: "120 grams",
+                 genre: "Dance",
+                 price: "$23.13",
+                 image: "https://images-na.ssl-images-amazon.com/images/I/81VfiYEgSrL._SL1400_.jpg"
+                 )
+
+Product.create(artist: "Future",
+                 album: "DS2",
+                 year: "2015",
+                 weight: "120 grams",
                  genre: "Rap",
-                 price: "$21.99",
-                 image: "https://upload.wikimedia.org/wikipedia/en/0/07/BeastieBoysPaul%27sBoutique.jpg"
+                 price: "$27.99",
+                 image: "https://images-na.ssl-images-amazon.com/images/I/81QrmqJKJoL._SL1500_.jpg"
+                 )
+
+Product.create(artist: "Tame Impala",
+                 album: "Currents",
+                 year: "2015",
+                 weight: "120 grams",
+                 genre: "Alternative",
+                 price: "$23.99",
+                 image: "https://images-na.ssl-images-amazon.com/images/I/91sqAJFwmJL._SL1400_.jpg"
                  )
