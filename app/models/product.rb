95ae7c69922339a29825ac25e37a5bc6444867b3
degel55 <chicubfan@gmail.com>
2016-09-07 23:43:56 -0500

    EOD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  # def sale_message
+  #   if price.to_f > 20
+  #     "Discount Item!"
+  #   else  
+  #     "On Sale!"
+  #   end  
+  # end 
+
+  def tax
+    price.to_f * 0.09
+  end 
+
+  def total
+    price.to_f + tax
+  end
+
+  def discounted?
+    price.to_f < 20
+  end
 end
