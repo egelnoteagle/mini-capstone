9d2ecf946db623100529259f4b3023a362618ce8
degel55 <chicubfan@gmail.com>
2016-09-08 21:26:38 -0500

    added stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830014917) do
+ActiveRecord::Schema.define(version: 20160909004410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20160830014917) do
     t.string   "genre"
     t.string   "price"
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
